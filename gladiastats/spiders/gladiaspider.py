29a52b58b848b9f0159d6acb738d6c12382df814
Restoro <patrick.fritsch@hotmail.com>
2019-06-06 01:35:28 +0200

    First push

---
+++ gladiastats/spiders/gladiaspider.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GladiaspiderSpider(scrapy.Spider):
+    name = 'gladiaspider'
+
+    def start_requests(self):
+        self.current_results = 0
+        self.current_page = 1
+        self.max_results = int(getattr(self, 'max_r', 100))
+        urls = [
+            'https://stats.gladiabots.com/player?&display=matches&matchMode=1&name=ThordanSsoa&page=1'
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for matchresult in response.css('.result'):
+            if self.current_results < self.max_results:
+                self.current_results+=1
+
+                yield {
+                    'matchid': matchresult.css('td::text')[1].get(),
+                    'score_delta': matchresult.css('td::text')[4].get(),
+                    'current_elo': matchresult.css('td::text')[5].get().split()[0],
+                    'enemy_elo': matchresult.css('td.player i::text').get().strip(' ()'),
+                    'gained points': matchresult.css('td::text')[5].get().split()[1],
+                    'outcome': self.get_outcome(matchresult)
+                }
+                
+
+        if self.current_results < self.max_results:
+            self.current_page += 1
+            next_page = 'https://stats.gladiabots.com/player?&display=matches&matchMode=1&name=ThordanSsoa&page={}'.format(self.current_page)
+            print(next_page)
+            if next_page is not None:
+                yield response.follow(next_page, self.parse)
+    
+    def get_outcome(self, matchresult):
+        outcome_text = 'draw'
+        outcome = matchresult.css('.victory')
+        if not outcome:
+            outcome = matchresult.css('.draw')
+            if not outcome:
+                outcome_text = 'defeat'
+        else:
+            outcome_text = 'victory'
+        return outcome_text
+
